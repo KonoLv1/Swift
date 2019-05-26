5d148bb6a631908cb9039ddf2452e401a8ac7609
Junichi Koyama <1809106@s.asojuku.ac.jp>
2019-05-26 13:31:50 +0900

    Initial Commit

---
+++ Ruby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ruby
+//
+//  Created by 小山順一 on 2019/05/26.
+//  Copyright © 2019 小山順一. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
